6b73c990ebb4f144e146adb5771751ae124adb90
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-21 11:38:52 +0300

    add solution

--- src/pairs.js
+++ src/pairs.js
@@ -3,5 +3,10 @@ export const cons = (x, y) => f => f(x, y);
 
 // BEGIN (write your solution here)
 // @flow
+const T = (thn: mixed, els: mixed): mixed => thn; // eslint-disable-line
+const F = (thn: mixed, els: mixed): mixed => els; // eslint-disable-line
+
+export const car = cons(T);
+export const cdr = cons(F);
 
 // END
